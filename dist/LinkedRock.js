304d2c014cb4013a5ce3dc21b678b22e8e4c04fe
Hua Z <corona7@163.com>
2015-11-10 10:21:53 +0800

    优化代码
    使用原型继承的方式，将对象独立出来。

--- dist/LinkedRock.js
+++ dist/LinkedRock.js
@@ -34,113 +34,87 @@ var LinkedRock = (function (window, document) {
 			div.style.cssText = "position:relative;margin:0;padding:0;left:0;width:100%;height:50px;line-height:50px;";
 			div.style.borderBottom = "1px solid #E5E5E5";
 			this.moveArea.appendChild(div);
-			div.addEventListener("touchstart", _touchStart);
-			div.addEventListener("touchmove", _touchMove);
-			div.addEventListener("touchend", _touchEnd);
+			div.addEventListener("touchstart", this._touchStart);
+			div.addEventListener("touchmove", this._touchMove);
+			div.addEventListener("touchend", this._touchEnd);
 		};
 	}
 	
 	//The version
-	LinkedRock.version = "1.0.0";
-
+	// LinkedRock.version = "1.0.0";
 	var dragObj, newBlank;
 
-	/**
-	 * Define the event functions
-	 */
-	var _touchStart = LinkedRock._touchStart = function(e) {
-		e = e || event;
-		e.preventDefault();
-		dragObj = this;
-		var xy = getXY(dragObj);
-		dragObj.style.position = "absolute";
-		dragObj.style.top = xy.topSet + "px";
-		dragObj.style.width = xy.widthSet + "px";
-		dragObj.style.borderTop = "1px solid #E5E5E5";
-		dragObj.style.borderColor = "#363C62";
-		dragObj.style.opacity = "0.6";
-		newBlank = document.createElement("div");
-		newBlank.style.cssText = "width:" + xy.widthSet + "px;height:" + xy.heightSet+ "px;" + 
-			"border-top:1px dashed #363C62;border-bottom:1px dashed #363C62";
-		dragObj.parentNode.insertBefore(newBlank, dragObj);
-		return false;
-	}
-	var _touchMove = LinkedRock._touchMove = function(e) {
-		e = e || event;
-		e.preventDefault();
-		if (!dragObj) return false;
-		dragObj.style.top = (e.touches[0].clientY - dragObj.offsetHeight/2) + "px";
-		moveBlankDiv(e);
-	}
-	var _touchEnd = LinkedRock._touchEnd = function(e) {
-		e = e || event;
-		if (!dragObj) return false;
-		dragObj.parentNode.insertBefore(dragObj, newBlank);
-		dragObj.style.position = "";
-		dragObj.style.borderTop = "";
-		dragObj.style.borderColor = "#E5E5E5";
-		dragObj.parentNode.removeChild(newBlank);
-		dragObj.style.opacity = "1";
-		dragObj = {};
-	}
+	LinkedRock.prototype = {
+		constructor: LinkedRock,
+		version: "1.0.0",
+		getXY: function(e) {
+			var a = new Array();
+		    a.topSet = e.offsetTop; 
+		    a.widthSet = e.offsetWidth;
+		    a.heightSet = e.offsetHeight;
+		    while(e = e.offsetParent){
+			    a.topSet += e.offsetTop;
+		    }
+		    return a;
+		},
+		moveBlankDiv: function(e) {
+			var blocks = document.querySelectorAll("#moveArea-div-child");
+			for (var i = 0; i < blocks.length; i++) {
+				if (blocks[i] == dragObj) continue;
 
-	/**
-	 * Move the blank div accoring to the dragged div's position
-	 * @param  {Event object}
-	 * @return {null}
-	 */
-	var moveBlankDiv = LinkedRock.moveBlankDiv = function(e) {
-		var blocks = document.querySelectorAll("#moveArea-div-child");
-		for (var i = 0; i < blocks.length; i++) {
-			if (blocks[i] == dragObj) continue;
-			var moveMark = upOrDown(blocks[i], e);
-			if (moveMark == 0) 
-				continue;
-			else if (moveMark == 1) 
-				blocks[i].parentNode.insertBefore(newBlank, blocks[i]);
-			else {
-				if (blocks.length - 1 === i) 
-					blocks[i].parentNode.appendChild(newBlank);
-				else
-					blocks[i].parentNode.insertBefore(newBlank, blocks[i + 1]);
-			}
-			return;
-		};
+				var a = this.getXY(blocks[i]);
+			    var eClientY = e.touches[0].clientY;
+			    
+			    if ((eClientY > (a.topSet + a.heightSet * 0.5)) && (eClientY < (a.topSet + a.heightSet))) {
+			    	//向上移动
+		        blocks[i].parentNode.insertBefore(newBlank, blocks[i]);
+			    } else if ((eClientY > a.topSet) && (eClientY <= (a.topSet + a.heightSet * 0.5))) {
+			    	//向下移动
+			      if (blocks.length - 1 === i)
+						blocks[i].parentNode.appendChild(newBlank);
+					else
+						blocks[i].parentNode.insertBefore(newBlank, blocks[i + 1]);
+			    } else 
+			    	continue;
+				return;
+			};
+		},
+		_touchStart: function(e) {
+			e = e || event;
+			e.preventDefault();
+			dragObj = this;
+			var xy = LinkedRock.prototype.getXY(dragObj);
+			dragObj.style.position = "absolute";
+			dragObj.style.top = xy.topSet + "px";
+			dragObj.style.width = xy.widthSet + "px";
+			dragObj.style.borderTop = "1px solid #E5E5E5";
+			dragObj.style.borderColor = "#363C62";
+			dragObj.style.opacity = "0.6";
+			newBlank = document.createElement("div");
+			newBlank.style.cssText = "width:" + xy.widthSet + "px;height:" + xy.heightSet+ "px;" + 
+				"border-top:1px dashed #363C62;border-bottom:1px dashed #363C62";
+			dragObj.parentNode.insertBefore(newBlank, dragObj);
+			return false;
+		},
+		_touchMove: function(e) {
+			e = e || event;
+			e.preventDefault();
+			if (!dragObj) return false;
+			dragObj.style.top = (e.touches[0].clientY - dragObj.offsetHeight/2) + "px";
+			LinkedRock.prototype.moveBlankDiv(e);
+		},
+		_touchEnd: function(e) {
+			e = e || event;
+			if (!dragObj) return false;
+			dragObj.parentNode.insertBefore(dragObj, newBlank);
+			dragObj.style.position = "";
+			dragObj.style.borderTop = "";
+			dragObj.style.borderColor = "#E5E5E5";
+			dragObj.parentNode.removeChild(newBlank);
+			dragObj.style.opacity = "1";
+			dragObj = {};
+		}
 	}
-
-	/**
-	 * Check the way of moving
-	 * @param  {dragged object}
-	 * @param  {event object}
-	 * @return {moving direction}
-	 */
-	var upOrDown = LinkedRock.upOrDown = function(o, e) {
-		var xy = getXY(o);
-		var eClientY = e.touches[0].clientY;
-		if ((eClientY > (xy.topSet + xy.heightSet * 0.5)) && (eClientY < (xy.topSet + xy.heightSet)) 
-		    return 1; //UP
-		else if ((eClientY > xy.topSet) && (eClientY <= (xy.topSet + xy.heightSet * 0.5))) 
-		    return 2; //DOWN	
-		else 
-		    return 0; //NO MOVEING
-	}
-	
-	/**
-	 * Get the width,top,height of the dragged object
-	 * @param  {event object}
-	 * @return {array}
-	 */
-	var getXY = LinkedRock.getXY = function(e) {
-		var a = new Array();
-	    a.topSet = e.offsetTop; 
-	    a.widthSet = e.offsetWidth;
-	    a.heightSet = e.offsetHeight;
-	    while(e = e.offsetParent){
-		    a.topSet += e.offsetTop;
-	    }
-	    return a;
-	}
-
 	return LinkedRock;
 
 })(window, document);
